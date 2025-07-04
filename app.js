f5e3fe6d23dae4768dd43a158606c9bccd80af94
jingojam <xtian.arano1224@gmail.com>
2025-07-04 19:40:04 +0800

    init tree

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('express-handlebars')
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// MongoDB connection string        @LIAM replace this
+const mongoURI = 'mongodb://localhost:27017/mydatabase';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Serve static files (optional, useful for CSS/JS/images)
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Index', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
