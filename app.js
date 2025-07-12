a3368c0f2c493f669ae571c1f22e3600727c15b7
rainexee <157507549+rainexee@users.noreply.github.com>
2025-07-12 21:05:25 +0800

    test fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbars = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
-
+//TODO: Fix prof_info
 const app = express();
 const port = 3000;
 
