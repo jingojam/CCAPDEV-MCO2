2f1c3d353ac53b6e365f025bf1882f33a6ced397
rainexee <157507549+rainexee@users.noreply.github.com>
2025-07-14 03:24:46 +0800

    navbar

--- app.js
+++ app.js
@@ -82,6 +82,49 @@ app.get('/user/:id/image', async (req, res) => {
   }
 });
 
+app.post('/search-user', async (req, res) => {
+  try {
+    const fullName = req.body.full_name.trim();
+
+    const [firstName, ...rest] = fullName.split(/\s+/);
+    const lastName = rest.join(' ');
+
+    if (!firstName || !lastName) {
+      return res.send(`
+        <script>
+          alert("Please enter both first and last name.");
+          window.history.back();
+        </script>
+      `);
+    }
+
+    const user = await User.findOne({
+      first_name: new RegExp('^' + firstName + '$', 'i'),   // case insensitive
+      last_name: new RegExp('^' + lastName + '$', 'i')
+    });
+
+    if (!user) {
+      return res.send(`
+        <script>
+          alert("User with that name not found.");
+          window.history.back();
+        </script>
+      `);
+    }
+
+    // Redirect to profile using user._id
+    return res.redirect(`/prof_info?userId=${user._id}`);
+  } catch (err) {
+    console.error(err);
+    return res.send(`
+      <script>
+        alert("An error occurred while searching. Please try again.");
+        window.history.back();
+      </script>
+    `);
+  }
+});
+
 
 // Mount routers
 app.use('/sign_in', signinRoute);
