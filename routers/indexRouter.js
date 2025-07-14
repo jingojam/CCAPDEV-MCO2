197e95956566f6867c710bc94c19228c1f73ac24
Liam Ancheta <liam_michael_ancheta@dlsu.edu.ph>
2025-07-14 23:31:02 +0800

    routing for indices cuz life can't be easy

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const indexRouter = express.Router();
+const router  = express.Router();
+const User    = require('../model/userRegistry'); // Mongoose model
 
-indexRouter.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Index'
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    // Use any unique field that survives reseeding
+    const student = await User.findOne({ email: 'john.doe@dlsu.edu.ph' }).lean();
+    const tech    = await User.findOne({ email: 'admin.user@dlsu.edu.ph' }).lean();
+
+    if (!student || !tech) {
+      return res.status(500).send('Seed users not found.'); // or seed them on the fly
+    }
+
+    res.render('index', {
+      studentId: student._id.toString(),
+      techId:    tech._id.toString()
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
-module.exports = indexRouter;
+module.exports = router;
